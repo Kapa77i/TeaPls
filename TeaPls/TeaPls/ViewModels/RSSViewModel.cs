17ec213608fddace885b30fc0fddb3222d5d286b
Simo <76773814+Hovari@users.noreply.github.com>
2023-02-07 00:21:17 +0200

    Jos joku keksii miten ton RSSPage saa siihen valikkoon, niin tarjoon tikkarin.

---
+++ TeaPls/TeaPls/ViewModels/RSSViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TeaPls.ViewModels
+{
+    public class RSSViewModel : BaseViewModel
+    {
+
+        public RSSViewModel()
+        {
+            Title = "RSS";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+
+        }
+        public ICommand OpenWebCommand { get; }
+    }
+}
