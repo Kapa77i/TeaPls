8fcf996c169f75e876ada516a2013fe6f33ce8f8
Sebastian-1995 <81906311+Sebastian-1995@users.noreply.github.com>
2023-02-06 18:43:56 +0200

    Project folder

---
+++ TeaPls/TeaPls/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TeaPls.Services;
+using TeaPls.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TeaPls
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TeaPls/TeaPls/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TeaPls.ViewModels;
+using Xamarin.Forms;
+
+namespace TeaPls.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
