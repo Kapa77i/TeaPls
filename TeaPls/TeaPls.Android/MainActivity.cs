aa1d4ec55b82284eba274be1b705d9c0990d238c
Lauri Hartikainen <103640313+Jokapaikanlauri@users.noreply.github.com>
2023-02-09 14:59:06 +0200

    plugin.media nuget asennettu ja kamera aukeaa mutta ei tallenna kuvaa mihinkään

--- TeaPls/TeaPls.Android/MainActivity.cs
+++ TeaPls/TeaPls.Android/MainActivity.cs
@@ -2,6 +2,9 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Plugin.Media;
+using Android.Views;
+using Android.Widget;
 
 namespace TeaPls.Droid
 {
@@ -11,12 +14,13 @@ namespace TeaPls.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            CrossMedia.Current.Initialize();
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
--- TeaPls/TeaPls/Views/NewItemPage.xaml.cs
+++ TeaPls/TeaPls/Views/NewItemPage.xaml.cs
@@ -5,17 +5,66 @@ using TeaPls.Models;
 using TeaPls.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Media;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using System.IO;
 
 namespace TeaPls.Views
 {
     public partial class NewItemPage : ContentPage
     {
         public Item Item { get; set; }
+        public string PhotoPath { get; private set; }
 
         public NewItemPage()
         {
             InitializeComponent();
             BindingContext = new NewItemViewModel();
+
+
+            takePhoto.Clicked += async (sender, args) =>
+            {
+                await TakePhotoAsync();
+                async Task TakePhotoAsync()
+                {
+                    try
+                    {
+                        var photo = await MediaPicker.CapturePhotoAsync();
+                        await LoadPhotoAsync(photo);
+                        Console.WriteLine($"CapturePhotoAsync COMPLETED: {photo}");
+                    }
+                    catch (FeatureNotSupportedException fnsEx)
+                    {
+                        // Feature is not supported on the device
+                    }
+                    catch (PermissionException pEx)
+                    {
+                        // Permissions not granted
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+                    }
+                }
+
+                async Task LoadPhotoAsync(FileResult photo)
+                {
+                    // canceled
+                    if (photo == null)
+                    {
+                        photo = null;
+                        return;
+                    }
+                    // save the file into local storage
+                    var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+                    using (var stream = await photo.OpenReadAsync())
+                    using (var newStream = File.OpenWrite(newFile))
+                        await stream.CopyToAsync(newStream);
+
+                    PhotoPath = newFile;
+                }
+            };
         }
     }
 }
