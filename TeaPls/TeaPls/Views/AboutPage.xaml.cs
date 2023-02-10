48cd69ce214c99d2792e8583f68a68b1c70a2f46
Simo <76773814+Hovari@users.noreply.github.com>
2023-02-10 18:05:06 +0200

    Lisäsin nappulan, joka antaa uuden tee-suosituksen.

--- TeaPls/TeaPls/Views/AboutPage.xaml.cs
+++ TeaPls/TeaPls/Views/AboutPage.xaml.cs
@@ -12,15 +12,23 @@ namespace TeaPls.Views
         public AboutPage()
         {
             InitializeComponent();
+            RandomTeaGenerator();
+
+        }
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            //Tämä on vain nappia varten, joka luo uuden nimen teelle..
+            RandomTeaGenerator();
+        }
+        private void RandomTeaGenerator()
+        {
             //Arvotaan randomi väri ja adjektiivi
             Random random = new Random();
             int RandomColor = random.Next(0, colors.Length);
             int RandomAdjective = random.Next(0, adjectives.Length);
             //muodostetaan noilla randomeilla uusi tee ja syötetään se frinttiin HotTea nimiseen elementtiin "frontissa".
             HotTea.Text = $"{colors[RandomColor]} {adjectives[RandomAdjective]}";
-
         }
-
 
     }
 }
