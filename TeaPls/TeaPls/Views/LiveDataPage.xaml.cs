629f8ba56e977a9fb09250d89dadf77fef26bbbf
Simo <76773814+Hovari@users.noreply.github.com>
2023-02-12 19:47:33 +0200

    Illan viimein hidas

--- TeaPls/TeaPls/Views/LiveDataPage.xaml.cs
+++ TeaPls/TeaPls/Views/LiveDataPage.xaml.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using TeaPls.ViewModels;
 
 namespace TeaPls.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LiveDataPage : ContentPage
     {
-  
+
         public LiveDataPage()
         {
             InitializeComponent();
