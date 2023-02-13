74fb32e216fad9ab693d064f83c8d864220dd3b2
Lauri Hartikainen <103640313+Jokapaikanlauri@users.noreply.github.com>
2023-02-13 17:00:28 +0200

    TakePhotoAsync funktio läpsästy ulkopuolelle

--- TeaPls/TeaPls/Views/NewItemPage.xaml.cs
+++ TeaPls/TeaPls/Views/NewItemPage.xaml.cs
@@ -26,45 +26,47 @@ namespace TeaPls.Views
             takePhoto.Clicked += async (sender, args) =>
             {
                 await TakePhotoAsync();
-                async Task TakePhotoAsync()
-                {
-                    try
-                    {
-                        var photo = await MediaPicker.CapturePhotoAsync();
-                        await LoadPhotoAsync(photo);
-                        Console.WriteLine($"CapturePhotoAsync COMPLETED: {photo}");
-                    }
-                    catch (FeatureNotSupportedException fnsEx)
-                    {
-                        // Feature is not supported on the device
-                    }
-                    catch (PermissionException pEx)
-                    {
-                        // Permissions not granted
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
-                    }
-                }
-
-                async Task LoadPhotoAsync(FileResult photo)
-                {
-                    // canceled
-                    if (photo == null)
-                    {
-                        photo = null;
-                        return;
-                    }
-                    // save the file into local storage
-                    var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
-                    using (var stream = await photo.OpenReadAsync())
-                    using (var newStream = File.OpenWrite(newFile))
-                        await stream.CopyToAsync(newStream);
-
-                    PhotoPath = newFile;
-                }
+               
             };
+
+        }
+        async Task TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync();
+                await LoadPhotoAsync(photo);
+                Console.WriteLine($"CapturePhotoAsync COMPLETED: {photo}");
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature is not supported on the device
+            }
+            catch (PermissionException pEx)
+            {
+                // Permissions not granted
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CapturePhotoAsync THREW: {ex.Message}");
+            }
+        }
+
+        async Task LoadPhotoAsync(FileResult photo)
+        {
+            // canceled
+            if (photo == null)
+            {
+                photo = null;
+                return;
+            }
+            // save the file into local storage
+            var newFile = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+            using (var stream = await photo.OpenReadAsync())
+            using (var newStream = File.OpenWrite(newFile))
+                await stream.CopyToAsync(newStream);
+
+            PhotoPath = newFile;
         }
     }
 }
